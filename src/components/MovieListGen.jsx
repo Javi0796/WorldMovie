b714735fe3da3040270ee789d2632903ff77f03c
Javier <javirios961107@gmail.com>
2023-10-30 16:39:52 -0500

    images and deploy list

--- src/components/MovieListGen.jsx
+++ src/components/MovieListGen.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MovieListGen = ( { listGen } ) => {
+    var image = ''
+
+    const getImg = (i) => {
+        image =`src/assets/${i+1}.jpg`
+    }
+
     return (
-        <div className='card_container row row-cols-2'>
+        <div className='card_container'>
             {listGen?.map((o,i)=>
-                <div className='card_gen col-md' key={i}>
-                    <img className='card-img-top' src='/' alt='card'/>
+                <div className='card_gen' key={i}>
+                    {getImg(i)}
+                    <img className='card-img-top' src={image} alt='card'/>
                     <div className='card-body'>
                     <p className='card-text'>{`${o.name}`}</p>
                 </div>
