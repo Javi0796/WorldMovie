d780006734a574c705487d86ce09275f62e36602
Javier <javirios961107@gmail.com>
2023-10-31 12:13:54 -0500

    environment variable

--- src/models/Options.js
+++ src/models/Options.js
@@ -2,8 +2,8 @@ const OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjYxMzE5ZDA0NGU5MDQ0ODIyMWM2ZWNlYzA5MzJkNiIsInN1YiI6IjY1MjlhNDRkMzU4ZGE3MDBlM2JjYWU3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nukD30TYN4lUcelIQMKhC3iiYiToktPUtPLFm1p2njo'
+      Authorization: import.meta.env.VITE_DBMOVIE_APP_KEY,
     }
   }
 
-  export default OPTIONS;
+export default OPTIONS;
