d34a1fa098b870ab3e9f737d5d67dd64bad48fe3
Javi0796 <39526092+Javi0796@users.noreply.github.com>
2024-07-23 14:56:11 -0500

    pagination map

--- src/components/pure/PaginedMovies.jsx
+++ src/components/pure/PaginedMovies.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useTransition} from 'react';
+import React, {useState, useTransition, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 
@@ -8,19 +8,35 @@ const PaginedMovies = ( { items, itemsPerPage, length }) => {
 
     const totalPage = Math.ceil(length / itemsPerPage);
 
-    const handleClick = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
+    const [imgList, setImgList] = useState(currentPage-1);
+
+    const changePage = ((page) => {
+        setCurrentPage(page)
+        window.scrollTo({
+            top: 0,
+          })
+    })
+
+    useEffect(() => {
+      setImgList((currentPage*itemsPerPage)-1)
+      }, [currentPage])
 
     return (
         <div>
-            <div className="items">
-                
-            </div>
-            <div className="pagination">
-
-            </div>
-        </div>
+          {items?.slice(imgList, itemsPerPage).map((o, i) => {
+          <div className='movie-list-gen' key={i}>
+            <img src={`https://image.tmdb.org/t/p/w500${o.poster}`}></img>
+          </div>
+        })}
+          <div className='button-bar'>
+            {Array.from({ length: totalPage }, (_, index) => (
+              <button className={`page-button page-${index+1}`} key={index+1} style={{backgroundColor: index+1 === currentPage ?
+              '#BBCCDE' : '#88A7C6', border: '1px solid #000', display : 'flex', alignItems : 'center', justifyContent : 'center'}} onClick={() => changePage(index + 1) }>
+              {index + 1}
+            </button>
+            ))}
+          </div>
+        </div> 
     );
 };
 
